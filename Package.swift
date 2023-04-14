6da40fcc6d5f007042ff099be593467ef4192064
Piotr Wilk <piotr.wilk@conjure.co.uk>
2023-04-14 11:55:02 +0100

    Add modules for Circle and triangle

--- Package.swift
+++ Package.swift
@@ -4,15 +4,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "UILibrary",
+    name: "UI-Library",
     platforms: [
         .iOS(.v15)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "UILibrary",
-            targets: ["UILibrary"]),
+            name: "UI-Library",
+            targets: ["Library", "Circle", "Triangle"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -22,8 +22,14 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "UILibrary",
+            name: "Library",
             dependencies: []),
+        .target(
+            name: "Triangle",
+            dependencies: [],
+        .target(
+            name: "Circle",
+            dependencies: [],
         .testTarget(
             name: "UILibraryTests",
             dependencies: ["UILibrary"]),
